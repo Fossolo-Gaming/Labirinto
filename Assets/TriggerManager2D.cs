d746b9c54b09ff4024b621afc5de9ac28df1fe45
Shachar Oz <shachar.oz@gmail.com>
2019-10-26 08:58:53 +0200

    labirinth prepared for multi levels

---
+++ Assets/TriggerManager2D.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerManager2D : MonoBehaviour
+{
+
+    public bool IsTrigger;
+    public string TargetObjectTagName = "Target";
+    
+    public UnityEngine.Events.UnityEvent OnHit;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (IsTrigger && collision.tag == TargetObjectTagName)
+            OnHit.Invoke();
+    }
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+}
